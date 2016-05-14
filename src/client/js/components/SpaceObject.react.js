dc10b12a691d5773c7c778ef12f2ec2d4c940286
Michal Mašek <misamasek@gmail.com>
2016-05-14 19:53:25 +0200

    Add shadow objects when object overlaps edge of the screen

--- src/client/js/components/SpaceObject.react.js
+++ src/client/js/components/SpaceObject.react.js
@@ -11,40 +11,50 @@ class SpaceObject extends React.Component {
     };
   }
 
-  _onClick(event) {
-    event.stopPropagation();
-    const newState = {
-      isSelected: !this.state.isSelected,
-    };
-    this.setState(newState);
-  }
-
-  render() {
-    const size = this.props.hull.get("size") * SpaceConstants.SPACE_SIZE;
-    const rotation = (this.props.position.get("r") * 360) % 360; // degrees
-    const components = [];
-    this.props.hull.get("components").forEach((component, index) => {
-      components.push(<Component key={index} component={component} />);
-    });
-
+  _createObject(components, size, x, y, rotation, key) {
     const style = {
-      left: `${this.props.position.get("x") * 100}%`,
-      top: `${this.props.position.get("y") * 100}%`,
+      left: `${x * 100}%`,
+      top: `${y * 100}%`,
       width: `${size}rem`,
       height: `${size}rem`,
       marginTop: `${-size / 2}rem`,
       marginLeft: `${-size / 2}rem`,
       transform: `rotate(${rotation}deg)`,
     };
-    const onClick = this._onClick.bind(this);
-    return (
-      <span className="object"
-        style={style}
-        onClick={onClick}
-      >
-        {components}
-      </span>
-    );
+    return <span className="object" key={key} style={style}>{components}</span>;
+  }
+
+  render() {
+    const s = this.props.hull.get("size");
+    const x = this.props.position.get("x");
+    const y = this.props.position.get("y");
+    const r = this.props.position.get("r");
+
+    const size = s * SpaceConstants.SPACE_SIZE;
+    const rotation = (r * 360) % 360; // degrees
+    const components = [];
+    this.props.hull.get("components").forEach((component, index) => {
+      components.push(<Component key={index} component={component} />);
+    });
+
+    const objects = [];
+
+    // Add real object.
+    objects.push(this._createObject(components, size, x, y, rotation, "real"));
+    // Add shadow objects for each edge that the real object overlaps.
+    if (x + s / 2 > 1.0) {
+      objects.push(this._createObject(components, size, x - 1.0, y, rotation, "right"));
+    }
+    if (y + s / 2 > 1.0) {
+      objects.push(this._createObject(components, size, x, y - 1.0, rotation, "bottom"));
+    }
+    if (x - s / 2 < 0.0) {
+      objects.push(this._createObject(components, size, x + 1.0, y, rotation, "left"));
+    }
+    if (y - s / 2 < 0.0) {
+      objects.push(this._createObject(components, size, x, y + 1.0, rotation, "top"));
+    }
+    return <span>{objects}</span>;
   }
 }
 
