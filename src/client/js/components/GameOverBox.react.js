23313be8b53360bbf4071667eff8c6a55aff82d2
Michal Mašek <misamasek@gmail.com>
2016-06-04 10:36:07 +0200

    Show score and hi score in Game over box

--- src/client/js/components/GameOverBox.react.js
+++ src/client/js/components/GameOverBox.react.js
@@ -10,6 +10,8 @@ class GameOverBox extends React.Component {
     const message = "Press any key to continue.";
     return (<div className="messageBox">
       <h1>{title}</h1>
+      <p><span>Score:</span></p><p><span className="score">{this.props.score}</span></p>
+      <p><span>Hi Score:</span></p><p><span className="score">{this.props.hiScore}</span></p>
       <p>{message}</p>
     </div>);
   }
