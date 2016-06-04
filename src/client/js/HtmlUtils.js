555d809a71026449ca06b28bd52dd7f689f305fd
Michal Mašek <misamasek@gmail.com>
2016-06-04 13:32:06 +0200

    Remove log statements

--- src/client/js/HtmlUtils.js
+++ src/client/js/HtmlUtils.js
@@ -8,14 +8,11 @@ const HtmlUtils = {
   },
 
   setToLocalStorage(key, value) {
-    console.log("set", key, value);
     window.localStorage.setItem(key, value);
   },
 
   getFromLocalStorage(key) {
-    const value = window.localStorage.getItem(key);
-    console.log("get", key, value);
-    return value;
+    return window.localStorage.getItem(key);
   },
 };
 
