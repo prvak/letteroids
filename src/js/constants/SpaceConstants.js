7267fa58693bb9dc9f4a75f835e6e2651bad8264
Michal Mašek <misamasek@gmail.com>
2016-06-20 21:57:58 +0200

    Do not shoot and rotate so fast

--- src/js/constants/SpaceConstants.js
+++ src/js/constants/SpaceConstants.js
@@ -2,13 +2,13 @@ const SpaceConstants = {
   // Space is a square. This is the length of its side in rem units.
   SPACE_SIZE: 50,
   // Ship rotation speed in full rotations per second.
-  SHIP_ROTATION_SPEED: 0.6,
+  SHIP_ROTATION_SPEED: 0.5,
   // Acceleration in screens per second^2.
-  SHIP_ACCELERATION: 0.3,
+  SHIP_ACCELERATION: 0.25,
   // Number of shots fired per second.
-  SHIP_SHOOTING_SPEED: 10,
+  SHIP_SHOOTING_SPEED: 7,
   // How many frames per second to draw.
-  FPS: 30,
+  FPS: 25,
   // How many milliseconds does the shot live. Shots disappear after this period.
   SHOT_TTL: 3000,
   // Speed of a shot in screens per second. Speed is relative to ship's speed.
